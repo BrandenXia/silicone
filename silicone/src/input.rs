d2f968d2cb0c95da3961f09b289d95c85ffcf8bf
BrandenXia <xxtbranden@outlook.com>
2025-03-16 16:56:24 -0700

    feat: input handling

--- silicone/src/input.rs
+++ silicone/src/input.rs
@@ -1,18 +1,34 @@
 use std::sync::{Arc, mpsc::Sender};
 
+use crossterm::event;
+use crossterm::event::EnableMouseCapture;
+use crossterm::event::Event;
+
 use crate::handler_default_new;
-use crate::state::{Event, Handler, State};
+use crate::state;
+use crate::state::{Handler, State};
 
 pub struct InputHandler;
 
 impl Handler for InputHandler {
     handler_default_new!();
 
-    fn deps(&self) -> &[Event] {
-        &[Event::Start]
+    fn deps(&self) -> &[state::Event] {
+        &[state::Event::Start]
     }
 
-    fn thread(&self, state: Arc<State>, tx: Sender<Event>) -> anyhow::Result<()> {
-        Ok(())
+    fn thread(&self, state: Arc<State>, tx: Sender<state::Event>) -> anyhow::Result<()> {
+        crossterm::execute!(std::io::stdout(), EnableMouseCapture)?;
+
+        let browser = &state.browser;
+        loop {
+            match event::read()? {
+                Event::Key(k) => browser.handle_key(k)?,
+                _ => continue,
+            }
+
+            tx.send(state::Event::RefreshScreen)
+                .expect("Failed to send refresh event");
+        }
     }
 }
